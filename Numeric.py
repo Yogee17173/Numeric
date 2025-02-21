def2f0999a98b0dbbb4cc6d02c9a2e47ce7ef5af
YOGEESH K P <yogeesh793@gmail.com>
2025-02-22 00:29:53 +0530

    Add files via upload

---
+++ Numeric.py
@@ -0,0 +1,38 @@
+# Python Numeric Operators
+a=21
+b=10
+
+# addition
+print("a+b:", a+b)
+
+# subtraction
+print("a-b:", a-b)
+
+# multiplication
+print("a*b:", a*b)
+
+# division
+print("a/b:", a/b)
+
+# modulus
+print("a%b:", a%b)
+
+# exponentiation
+print("a**b:", a**b)
+
+# floor division
+print("a//b:", a//b)
+
+# Python Floor Division Assignment
+a//=3
+print("a//=3:",a)
+
+# Python Exponentiation Assignment
+a**=3
+print("a**=3:",a)
+
+# Python Remainder Assignment
+a%=3
+print("a%=3:",a)
+
+
